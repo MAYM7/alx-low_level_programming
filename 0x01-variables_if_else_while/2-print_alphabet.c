11162d240d34c92200e674f7b96b3ff65afb7698
MAYM7 <mmkoko8@gmail.com>
2023-09-08 08:19:16 -0700

    done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - prints alphabet in lowecase.
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+	for (int i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
