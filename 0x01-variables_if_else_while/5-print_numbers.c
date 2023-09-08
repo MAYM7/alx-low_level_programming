256cc3fa470253946282b1cb64b33033a872e8a9
MAYM7 <mmkoko8@gmail.com>
2023-09-08 09:01:18 -0700

    done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 to 9.
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+	int a[10] = "0123456789";
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(a[i]);
+	}
+	putchar('\n');
+	return (0);
+}
