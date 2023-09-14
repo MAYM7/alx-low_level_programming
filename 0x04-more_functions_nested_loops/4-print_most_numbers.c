f3818954cb4b5039cc748fd928b96a724b827082
MAYM7 <mmkoko8@gmail.com>
2023-09-14 07:07:00 -0700

    done

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,11 +11,10 @@ void print_most_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		if (i = 2 && 4)
+		if (i != 2 && 4)
 		{
-			i++;
+			_putchar(i + '0');
 		}
-		_putchar(i + '0');
 	}
 	_putchar ('\n');
 }
