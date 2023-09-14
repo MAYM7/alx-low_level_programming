ec9533c182eccf734f9d8cd45fccfdd9e112a6c0
MAYM7 <mmkoko8@gmail.com>
2023-09-14 12:24:00 -0700

    done

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,17 +13,15 @@ void print_number(int n)
 		_putchar('-');
 	}
 
-	int d;
+	int d, j;
 
-	while (d = 1; d < n / 10)
+	while (d = 1; d <= n / 10)
 	{
 		d = d * 10;
 	}
 
 	while (d > 0)
 	{
-		int j;
-
 		j = n / d;
 		_putchar(j + '0');
 		n = n % d;
