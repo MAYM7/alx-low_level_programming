6dba25751655b680bb0a8965a7748a492664ab65
MAYM7 <mmkoko8@gmail.com>
2023-09-18 14:08:33 -0700

    done

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,7 +17,7 @@ void puts_half(char *str)
 	{
 		j /= 2;
 
-		for (i = 0; i >= j; i++)
+		for (i = 0; i <= j; i++)
 		{
 			_putchar(str[i]);
 		}
@@ -27,7 +27,7 @@ void puts_half(char *str)
 		j--;
 		j /= 2;
 
-		for (i = 0; i >= j; i++)
+		for (i = 0; i <= j; i++)
 	{
 			_putchar(str[i]);
 	}
