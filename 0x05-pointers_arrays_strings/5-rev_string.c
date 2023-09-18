e89b6a9d48fb037a3d75be4ae78fa28a62f8a25e
MAYM7 <mmkoko8@gmail.com>
2023-09-18 13:20:54 -0700

    done

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
 
 /**
  * rev_string - a function that prints a string in rev.
@@ -8,37 +10,15 @@
 void rev_string(char *s)
 {
 	int i, j;
-	char w[50];
+	char w;
 
-	i = 0;
-	while (s[i] != '\0')
+	j = strlen(s);
+
+	for (i = 0; i < j / 2; i++)
 	{
-	i++;
-	}
-	i = i - 1;
-	while (i >= 0)
-	{
-		w = s[i];
-		i--;
-	}
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	i = i - 1;
-	while (w[i] != '\0')
-	{
-		for (w[i] != ' ')
-		{
-			i--;
-		}
-		j = i - 1;
-		while (w[i] != '\0' || w[i] != ' ')
-		{
-		_putchar(w[i]);
-		i++;
-		}
-		i = j;
+	w = s[i];
+	s[i] = s[j - i - 1];
+	s[len - i - 1] = w;
 	}
 	_putchar('\n');
 }
