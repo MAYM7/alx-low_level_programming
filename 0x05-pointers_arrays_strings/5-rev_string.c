513b7380b69d010e2c90c526d4f071dd2921fe2e
MAYM7 <mmkoko8@gmail.com>
2023-09-18 13:07:47 -0700

    done

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * rev_string - a function that prints a string in rev.
+ *
+ * @s: pointer to the string to print reverced.
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char w[50];
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+	i++;
+	}
+	i = i - 1;
+	while (i >= 0)
+	{
+		s[i] = char w;
+		i--;
+	}
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i = i - 1;
+	while (w[i] != '\0')
+	{
+		for (w[i] != ' ')
+		{
+			i--;
+		}
+		j = i - 1;
+		while (w[i] != '\0' || w[i] != ' ')
+		{
+		_putchar(w[i]);
+		i++;
+		}
+		i = j;
+	}
+	_putchar('\n');
+}
