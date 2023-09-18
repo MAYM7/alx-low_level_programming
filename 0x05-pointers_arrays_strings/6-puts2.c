292e908c803294a3e71e314997a0d3dea85635b0
MAYM7 <mmkoko8@gmail.com>
2023-09-18 13:34:04 -0700

    done

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - a function that prints every other char in a string.
+ *
+ * @str: pointer to the string to print
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
