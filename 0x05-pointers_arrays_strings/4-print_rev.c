bd771a6b686e2cfa970baccfb08ddad3dc24dbab
MAYM7 <mmkoko8@gmail.com>
2023-09-18 12:18:28 -0700

    done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that prints a string in rev.
+ *
+ * @s: pointer to the string to print reverced.
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+	{
+	i++
+	}
+	for (i = i; str[i] != '\0'; i--)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
