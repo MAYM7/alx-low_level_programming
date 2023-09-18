77d53bbed595e5444ba03bb5ebb025634df073bd
MAYM7 <mmkoko8@gmail.com>
2023-09-18 12:38:47 -0700

    done

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,15 +9,15 @@ void print_rev(char *s)
 {
 	int i;
 
-	i = -1;
+	i = 0;
 	while (s[i] != '\0')
 	{
 	i++;
 	}
 	while (i >= 0)
 	{
-		_putchar(s[i]);
 		i--;
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
