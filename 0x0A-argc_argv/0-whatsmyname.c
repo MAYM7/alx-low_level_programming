869b12d0360447560f44f3b758a2b6c387fa4148
MAYM7 <mmkoko8@gmail.com>
2023-10-02 10:52:28 -0700

    done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 
 /**
- * mynameis - a program that prints its name, followed by a new line.
+ * main - a program that prints its name, followed by a new line.
  * @argc: the length of the string of arguments entered.
  * @argv: the array of arguments entered.
  * Return: Always 0.
  */
+
 int main(int argc, char **argv)
 {
 	printf("%s", *argv[0]);
