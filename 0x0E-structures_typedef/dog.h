d97c2969e6747f873722708984a232eff9fdde5f
MAYM7 <mmkoko8@gmail.com>
2023-10-10 13:58:05 -0700

    done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog -Dog data.
+ * @name: Name of the dog
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ * Desctiption: a structure with the name, age and owner of a dog.
+**/
 #typedef struct dog
 {
 	char *name;
