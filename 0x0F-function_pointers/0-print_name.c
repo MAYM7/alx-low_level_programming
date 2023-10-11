c505511db4e489b0657e9fa940b70e15280bfc93
MAYM7 <mmkoko8@gmail.com>
2023-10-11 15:17:32 -0700

    done

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,15 +6,14 @@
  * @name: name of the person
  * @f: is pointer
  * Return: Nothing.
-**/
+ **/
+
 void print_name(char *name, void (*f)(char *))
 {
+
 if (name == NULL || f == NULL)
-{
 return;
-}
-else
-{
+
 f(name);
-}
+
 }
