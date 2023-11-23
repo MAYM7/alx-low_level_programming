ef1884b3b759795d22bf327a976fc68c19aa834f
MAYM7 <mmkoko8@gmail.com>
2023-11-23 01:25:16 -0800

    done

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,13 +11,15 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int mask;
+	int bit_value;
+
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	unsigned long int mask = 0;
 
 	mask = 1UL << index;
-	int bit_value = (n & mask) ? 1 : 0;
+	bit_value = (n & mask) ? 1 : 0;
 
 	return (bit_value);
 }
